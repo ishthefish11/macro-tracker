18ddbfcc7a695dcf41637cbb63344e0b2bc4c107
ishthefish11 <ih66@njit.edu>
2023-11-14 16:51:03 -0500

    food class now stores a user object instead of a userId to identify the user.

--- src/main/java/com/macrotracker/macrotracker/Food/Food.java
+++ src/main/java/com/macrotracker/macrotracker/Food/Food.java
@@ -3,21 +3,18 @@ package com.macrotracker.macrotracker.Food;
 import com.macrotracker.macrotracker.User.User;
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
 public class Food {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long foodId;
     private String name;
-    private List<Double> nutrients; // index 0: protein | index 1: carbs | index 2: fats
+    private double[] nutrients = new double[3]; // index 0: protein | index 1: carbs | index 2: fats
     @ManyToOne
     private User user;
 
-    public Food(String name, Long userId) {
+    public Food(String name, User user) {
         this.name = name;
-        this.user = new User("", userId);
+        this.user = user;
         // add method for retrieving nutrients later when you implement the api
     }
 
@@ -29,11 +26,11 @@ public class Food {
         this.name = name;
     }
 
-    public List<Double> getNutrients() {
+    public double[] getNutrients() {
         return nutrients;
     }
 
-    public void setNutrients(List<Double> nutrients) {
+    public void setNutrients(double[] nutrients) {
         this.nutrients = nutrients;
     }
 
